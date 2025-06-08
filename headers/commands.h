174c20da7e9002da6bbc59233599994523c4f85d
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2025-06-08 11:39:30 +0530

    feat: added cd command to change between directories

--- headers/commands.h
+++ headers/commands.h
@@ -26,6 +26,7 @@ int quickoverflow_search(char **args); //open search
 int quickoverflow_help(char **args); //help command
 int quickoverflow_quit(char **args); //exit command
 int quickoverflow_about(char **args); //about command
+int quickoverflow_cd(char **args);
 
 /*
   List of builtin commands, followed by their corresponding functions.
@@ -35,13 +36,15 @@ char *builtin_str[] = {
   "help", //help command
   "quit", //exit command
   "about",
+  "cd",
 };
 
 int (*builtin_func[]) (char **) = {
   &quickoverflow_search, //search directory
   &quickoverflow_help, //help command
   &quickoverflow_quit, //exit command
-  &quickoverflow_about,
+  &quickoverflow_about, //about
+  &quickoverflow_cd, //change dir
 };
 
 int quickoverflow_num_builtins() {
@@ -51,6 +54,21 @@ int quickoverflow_num_builtins() {
 /*
 ***********************************Builtin function implementations*************************************
 */
+
+//Update 2.0.0 - Added cd for directory traversal
+
+//***************************************chdir*********************************************** */
+int quickoverflow_cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "QuickOverflow : expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("QuickOverflow");
+    }
+  }
+  return 1;
+}
 
 //***************************************sofsearch*********************************************** */
 //function for calling the python files for searching
@@ -72,7 +90,7 @@ int quickoverflow_search(char **args)
   PyRun_SimpleFile(fp, "search.py");
   fclose(fp);
   
-  //Update 1.1.0 removed Py_Finalise to avoid segmentation faults
+  //Update 2.0.0 removed Py_Finalise to avoid segmentation faults
   
   return 1;
 }
@@ -84,7 +102,7 @@ int quickoverflow_search(char **args)
 int quickoverflow_help(char **args){
   int i;
   printf("--------------------------------QuickOverflow-----------------------------\n");
-  printf("---------------------------------Version 1.0.1----------------------------\n");
+  printf("---------------------------------Version 2.0.0----------------------------\n");
   printf("Type program names and arguments, and hit enter.\n");
   printf("The following are built in:\n");
 
@@ -105,8 +123,8 @@ int quickoverflow_quit(char **args){
 
 int quickoverflow_about(char **args){
     printf("-------------------------------QuickOverflow-----------------------------------\n");
-    printf("Version:  1.0.1\n");
-    printf("License: MIT License\n");
+    printf("Version:  2.0.0\n");
+    printf("License: GNU GPL-3 License\n");
     printf("Author: Aryan Karamtoth (SpaciousCoder78)\n");
     printf("Author Email: aryankmmiv@outlook.com\n");
     printf("Supported Operating Systems: Linux\n");
